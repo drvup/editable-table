4e5e8a45527003b15ef98f0ce65d01462e9cde6c
cedricheisel <cedricheisel@192.168.60.25>
2015-12-28 10:00:57 +0100

    first test

--- editableTable.php
+++ editableTable.php
@@ -46,7 +46,7 @@ class editableTable {
         if(array_key_exists($name, $this->tableData[0])){            
             foreach($this->tableData as $row){
                 print_r($row);
-                $out .= '<div class="'.$classes.'">'.$row["$name"].'</div>';
+                $out .= '<div class="'.$classes.'">'.$row['$name'].'</div>';
             }
             return $out;
         }else{
